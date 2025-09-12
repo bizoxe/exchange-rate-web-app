e8473c7cd1b874f8c307adfa6adcf990d9d290bc
alexander <alex.matveev.dev@gmail.com>
2025-09-12 16:03:40 +0300

    test(currency_check_exists): add tests for currency_check_exists module

---
+++ tests/unit/test_currency_exists.py
@@ -0,0 +1,29 @@
+from unittest import IsolatedAsyncioTestCase
+from unittest.mock import (
+    AsyncMock,
+    patch,
+)
+
+from src.lib.currency_check_exists import CheckCurrencyExists
+
+from tests.data import data_helper
+
+AVAILABLE_CURRENCIES = data_helper.available_currencies
+
+
+class TestCurrencyExists(IsolatedAsyncioTestCase):
+    check_currency: CheckCurrencyExists
+
+    @classmethod
+    def setUpClass(cls) -> None:
+        cls.check_currency = CheckCurrencyExists()
+
+    @patch("aiohttp.ClientSession.get")
+    async def test_is_currency_exists(self, mock_get: AsyncMock) -> None:
+        mock_get.return_value.__aenter__.return_value.json = AsyncMock(return_value=AVAILABLE_CURRENCIES)
+        self.assertFalse(self.check_currency.cached_currencies)
+        res_true = await self.check_currency.is_currency_exists(currency="aud")
+        self.assertTrue(self.check_currency.cached_currencies)
+        self.assertTrue(res_true)
+        res_false = await self.check_currency.is_currency_exists(currency="blob")
+        self.assertFalse(res_false)
