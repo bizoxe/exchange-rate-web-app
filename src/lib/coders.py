b0495636496fb8700f6a349f85c1c4b85255a94d
alexander <alex.matveev.dev@gmail.com>
2025-09-17 13:32:47 +0300

    chore(coders): clean up unused code

--- src/lib/coders.py
+++ src/lib/coders.py
@@ -1,21 +1,10 @@
-from datetime import date
 from decimal import Decimal
-from typing import Any
 
 import msgspec
 
 encoder = msgspec.json.Encoder()
 
 decoder = msgspec.json.Decoder()
-
-
-def json_encode_default(obj: Any) -> str:  # noqa: ANN401
-    if isinstance(obj, date):
-        return obj.isoformat()
-    if isinstance(obj, Decimal):
-        return str(obj)
-    error_message = f"Object {type(obj)} is not serializable"
-    raise TypeError(error_message)
 
 
 json_encoder = encoder
